f1bc9597f6e8259028fe8966dbaccdcd1e8e675b
Peter Ellis <peter_c_ellis@hotmail.com>
2021-05-27 13:38:06 +0100

    first edit

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# lambda
+# Creating a Lambda function to run a bash shell script using Docker :whale2:
+
+ 
+## Intro 
+
+Lambda does not include shell scripts, so this document uses python to call a shell script. 
+You will need docker installed on your laptop and awscli access to the AWS account. 
+It’s best to get the script working before creating a new lambda image, because of the time involved in image deployment. 
+For this document the example project name will be project-rain 
+
+ 
+## Initial  Setup 
+
+Create a directory on your laptop called **"Your Project Name"**. Clone the repository, but push to a new project based repository.  
+Edit the script.sh file to include your code. The script.sh file needs to exit with **“/usr/local/bin/python -m awslambdaric app.handler”**, without this you’ll get a lambda execution error. Any error handling within the script can be done using AWS SNS. 
+If you need to install extra packages, add these to the apt-get install command in the Dockerfile. 
+
+
+## Bespoke Images 
+
+If you need to install extra packages and test them on the image, build the image with the ENTRYPOINT line commented out, this will allow you to logon to the image. Docker build it, start it and logon:
+``` 
+docker build -t project-rain .
+docker run -dt project-rain bash
+docker ps   (use the Name in the next command)
+docker exec -it <Container Name> /bin/bash
+```
+
+To stop and remove a container:
+```
+docker stop <Container Name>
+docker rm <Container Name>
+```
+To delete a Docker image:
+```
+docker images   (use the IMAGE ID in the next command)
+docker rmi -f <IMAGE ID>
+```
+
+## Build and Push to ECR
+
+You’ll need to create a ECR repository and make a note of the URL. 
+When the the script.sh is ready and if it needs to be, the Dockerfile has been edited, you're ready to build the image:
+```
+docker build -t project-rain .
+```
+Then tag it with the ECR URL:
+```
+docker tag project-rain 123456789012.dkr.ecr.eu-west-1.amazonaws.com/project-rain
+```
+You then need to logon to AWS
+```
+aws ecr get-login-password [--profile <PROFILE NAME>]| docker login --username AWS --password-stdin 123456789012.dkr.ecr.eu-west-1.amazonaws.com
+```
+And then you can push the image to ECR:
+```
+docker push 123456789012.dkr.ecr.eu-west-1.amazonaws.com/project-rain
+```
+
+
+## Create the Lambda Function 
+
+Select “Create function” and then select the “Container image” option, select the ECR image you pushed to the repository. The function needs a role attached to it, to give it any necessary AWS access.
+Remember to edit the memory and timeout settings, as the defaults are low.
+
+ 
+# APPENDIX 
+
+***Dockerfile*** 
+> The docker image build file.
+
+***app.py*** 
+> The app.handler python script. 
+
+***script.py*** 
+> The python startup script, that calls the shell script. 
+
+***script.sh*** 
+> The shell script, only the exit code included here.
+
--- script.sh
+++ script.sh
@@ -1,8 +1,99 @@
 #!/bin/bash
 
-#COMMANDS 
-<Your script> 
+### FUNCTIONS() ###
+take_backup() {
+	echo `date +%R`" Take backup..."
+	DESTIN=metermanager-live-mysql-rds-`date +%Y-%m-%d`
+	DBUCKET=s3://metermanager-backup-daily
+	WBUCKET=s3://metermanager-backup-weekly
 
-#EXIT 
-/usr/local/bin/python -m awslambdaric app.handler 
-exit 
+	export MYSQL_PWD=d8489dh498fh3489hf
+  mysqldump -h metermanager-live-mysql-rds-old.cdbxehdrfxi7.eu-west-1.rds.amazonaws.com -u metermanager --skip-extended-insert MOL2 | aws s3 cp - $DBUCKET/$DESTIN
+	#mysqldump -h metermanager-live-mysql-rds.cdbxehdrfxi7.eu-west-1.rds.amazonaws.com -u metermanager --skip-extended-insert MOL2 |gzip| aws s3 cp - $DBUCKET/$DESTIN
+	EX_CODE=`echo $?`
+
+	if [ "$EX_CODE" -gt "0" ] ; then
+		echo `date +%R`" Backup failed..."
+		aws sns publish --topic-arn arn:aws:sns:eu-west-1:902623068040:metermanager-slack --message "AWS Backup Account - backup command has failed!"
+		exit_script
+	fi
+	echo `date +%R`" Backup taken..."
+}
+
+check_backup() {
+	echo `date +%R`" Check_backup..."
+	COPIED=`aws s3 ls $DBUCKET |grep $DESTIN|awk '{print $4}'`
+	aws s3 ls $DBUCKET
+	if [ "$COPIED" = "$DESTIN" ] ; then
+		return
+	else
+		echo `date +%R`" No Match!"
+		aws sns publish --topic-arn arn:aws:sns:eu-west-1:902623068040:metermanager-slack --message "AWS Backup Account - backup copy has failed!"
+		exit_script
+	fi
+}
+
+remove_oldest_copy() {
+	echo `date +%R`" Remove oldest copy..."
+	BNUM=`aws s3 ls $DBUCKET |sort|wc -l`
+	FILE=`aws s3 ls $DBUCKET |sort|head -1|awk '{print $4}'`
+	echo $BNUM
+	if [ "$BNUM" -gt "5" ] ; then
+		echo `date +%R`" Delete oldest backup!"
+		aws s3 rm $DBUCKET/$FILE
+		DEL_ERR=`echo $?`
+		echo `date +%R`" aws s3 rm "$DBUCKET/$FILE
+		if [ "$DEL_ERR" -gt "0" ] ; then
+			aws sns publish --topic-arn arn:aws:sns:eu-west-1:902623068040:metermanager-slack --message "AWS Backup Account - remove oldest backup failed!"
+		fi
+	else
+		echo `date +%R`" None to delete!"
+		aws sns publish --topic-arn arn:aws:sns:eu-west-1:902623068040:metermanager-slack --message "AWS Backup Account - no oldest backup to delete!"
+	fi
+}
+
+weekly_copy () {
+	DAY=`date +%a`
+	if [ "$DAY" = "Wed"] ; then
+		echo `date +%R`" Weekly copy..."
+		aws s3 cp $DBUCKET/$DESTIN $WBUCKET/$DESTIN
+		COPY_ERR=`echo $?`
+		if [ "$COPY_ERR" -gt "0" ] ; then
+			aws sns publish --topic-arn arn:aws:sns:eu-west-1:902623068040:metermanager-slack --message "AWS Backup Account - weekly copy command has failed!"
+			exit_script
+		fi
+}
+
+remove_oldest_weekly_copy() {
+	echo `date +%R`" Remove oldest weekly copy..."
+	BNUM=`aws s3 ls $WBUCKET |sort|wc -l`
+	FILE=`aws s3 ls $WBUCKET |sort|head -1|awk '{print $4}'`
+	echo $BNUM
+	if [ "$BNUM" -gt "3" ] ; then
+		echo `date +%R`" Delete oldest backup!"
+		aws s3 rm $WBUCKET/$FILE
+		DEL_ERR=`echo $?`
+		echo `date +%R`" aws s3 rm "$WBUCKET/$FILE
+		if [ "$DEL_ERR" -gt "0" ] ; then
+			aws sns publish --topic-arn arn:aws:sns:eu-west-1:902623068040:metermanager-slack --message "AWS Backup Account - remove oldest weekly backup failed!"
+		fi
+	else
+		echo `date +%R`" None to delete!"
+		aws sns publish --topic-arn arn:aws:sns:eu-west-1:902623068040:metermanager-slack --message "AWS Backup Account - no oldest weekly backup to delete!"
+	fi}
+
+exit_script() {
+	echo `date +%R`" End the script..."
+	/usr/local/bin/python -m awslambdaric app.handler
+	exit
+}
+
+### The Script ###
+echo `date +%R`" Start the script...."
+take_backup
+check_backup
+remove_oldest_copy
+weekly_copy
+remove_oldest_weekly_copy
+exit_script
+
